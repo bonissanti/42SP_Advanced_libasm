1c098221a14c878212852a6a8b596eac75d21de2
Bruno Onissanti <brunoonissanti@gmail.com>
2025-09-06 17:56:11 -0300

    feat: Implement `ft_strcmp.s` and extend `main.c` with test case

--- mandatory/main.c
+++ mandatory/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 extern size_t ft_strlen(const char *str);
+extern int ft_strcmp(const char *s1, const char *s2);
 
 int main(void)
 {
@@ -11,10 +12,17 @@ int main(void)
         size_t expectedLen = strlen(msg);
 
         printf("Length of '%s': %zu\n", msg, len);
-        printf("Length of real strlen '%s': %zu\n", msg, expectedLen);
+        printf("Length of real strlen '%s': %zu\n\n", msg, expectedLen);
     }
     {
+        const char *msg1 = NULL;
+        const char *msg2 = NULL;
 
+        int diff = ft_strcmp(msg1, msg2);
+        // int diffExpected = strcmp(msg1, msg2);
+
+        printf("Result from ft_strcmp: %d\n", diff);
+        // printf("Result from original strcmp: %d\n", diffExpected);
     }
 
     return 0;
