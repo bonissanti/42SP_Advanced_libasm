8d3e6bb2e1bf46b32a54c6311cd020b049599660
Bruno Onissanti <brunoonissanti@gmail.com>
2025-09-22 20:22:43 -0300

    test: Update `ft_write` tests to use `ft_strlen` and handle edge cases

--- mandatory/tests/ft_write_test.c
+++ mandatory/tests/ft_write_test.c
@@ -2,6 +2,7 @@
 #include <criterion/criterion.h>
 
 extern ssize_t ft_write(int fd, const void *buffer, size_t count);
+extern size_t ft_strlen(const char *str);
 
 Test(ft_write, basic_write)
 {
@@ -9,9 +10,9 @@ Test(ft_write, basic_write)
     pipe(pipefd);
 
     const char *msg = "Hello, World!";
-    ssize_t result = ft_write(pipefd[1], msg, strlen(msg));
+    ssize_t result = ft_write(pipefd[1], msg, ft_strlen(msg));
 
-    cr_expect((size_t)result == strlen(msg));
+    cr_expect((size_t)result == ft_strlen(msg));
 
     close(pipefd[0]);
     close(pipefd[1]);
@@ -20,16 +21,15 @@ Test(ft_write, basic_write)
 Test(ft_write, invalid_fd)
 {
     const char *msg = "Hello, World!";
-    ssize_t result = ft_write(-1, msg, strlen(msg));
+    ssize_t result = ft_write(-1, msg, ft_strlen(msg));
 
     cr_expect(result == -1);
 }
 
-//TODO: fix this behavior
 Test(ft_write, null_string)
 {
     const char *msg = NULL;
-    ssize_t result = ft_write(1, msg, strlen(msg));
+    ssize_t result = ft_write(1, msg, 0);
 
     cr_expect(result == 0);
 }
