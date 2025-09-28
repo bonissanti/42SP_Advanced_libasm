42d79361b5ceeb7a1f4148b5d6b9e466909cae81
Bruno Onissanti <brunoonissanti@gmail.com>
2025-09-28 20:14:51 -0300

    refactor: Replace `main.c` with `libasm.h`, update Makefile to build `libasm.a`
    - Removed `main.c` and introduced `libasm.h` for more modular header management.
    - Updated Makefile to compile `libasm.a` instead of a program binary.
    - Adjusted Makefile targets for improved consistency and maintainability.

--- mandatory/libasm.h
+++ mandatory/libasm.h
@@ -1,8 +1,8 @@
-#include <errno.h>
+#ifndef LIBASM_H
+#define LIBASM_H
+
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 extern char *ft_strcpy(const char *dest, const char *src);
 extern size_t ft_strlen(const char *str);
@@ -11,17 +11,4 @@ extern ssize_t ft_write(int fd, const void *buffer, size_t count);
 extern ssize_t ft_read(int fd, const void *buffer, size_t count);
 extern char *ft_strdup(const char *src);
 
-int main(void)
-{
-    {
-        puts("######## FT_STRDUP TESTS ########");
-
-        const char *src = "Teste a ser duplicado";
-        char *dest = "";
-
-        dest = ft_strdup(src);
-
-        printf("Src is: %s and dest is: %s", src, dest);
-    }
-    return 0;
-}
+#endif //LIBASM_H
