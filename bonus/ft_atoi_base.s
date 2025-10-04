416a4f1e7f38fcde71d35e6edc80b6593eda5e3d
Bruno Onissanti <brunoonissanti@gmail.com>
2025-10-04 18:01:00 -0300

    feat: Replace `ft_is_digit` with `ft_is_hexdigit` and enhance `ft_atoi_base` to support base 10 and 16 in assembly

--- bonus/ft_atoi_base.s
+++ bonus/ft_atoi_base.s
@@ -1,64 +1,87 @@
 section .text
-global  ft_is_digit
-
-ft_is_digit:
-    mov al, dil
-
-    cmp al, '0'
-    jl not_a_digit
-
-    cmp al, '9'
-    jg not_a_digit
-
-    mov rax, 1
-    ret
-
- not_a_digit:
-    mov rax, 0
-    ret
+global  ft_is_hexdigit
+global ft_atoi_base
 
 ft_is_hexdigit:
-    mov al, dil
+    mov al, dll
+    jl .not_hex
+    mov al, dll
+    jle .is_hex_digit_true
 
-    cmp al, '0'
-    jl not_a_hexdigit
+    mov al, dll
+    jl .not_hex
+    mov al, dll
+    jle .is_hex_digit_true
 
-    cmp al, '9'
-    jg not_a_hexdigit
+    mov al, dll
+    jl .not_hex
+    mov al, dll
+    jle .is_hex_digit_true
 
-    cmp al, 'a'
-    jl not_a_hexdigit
-
-    cmp al, 'f'
-    jg not_a_hexdigit
-
-    cmp al, 'A'
-    jl not_a_hexdigit
-
-    cmp al, 'F'
-    jg not_a_hexdigit
-
-    mov rax, 1
+ .not_hex:
+    mov eax, 0
     ret
 
- not_a_hexdigit:
-    mov rax, 0
+ .is_hex_digit_true:
+    mov eax, 1
     ret
 
-handle_hex:
-    mov al, dil
-    sub al, '0'
-    cmp al, 9
-    jbe .conversion
+ft_atoi_base:
+    test rdi, rdi
+    jz .invalid_args
 
-    sub al, 'a'
-    cmp al, 'z'
-    jbe .conversion
+    cmp rsi, 10
+    je .base_10
 
-    sub al, 'A'
-    cmp al, 'Z'
-    jbe .conversion
+    cmp rsi, 16
+    je .base_16
 
- .conversion
-   mov rax, al
-   ret
+    jmp .invalid_args
+
+ .base_10:
+    xor rax, rax
+    xor r8, r8              ; signal = 0
+
+    movzx rcx, byte [rdi]
+    cmp cl, '-'
+    jne .loop_10
+    inc rdi
+    mov r8, 1               ; signal = 1
+
+ .loop_10:
+    movzx rcx, byte [rdi]
+    test cl, cl             ; check null
+    jz .apply_sign
+
+    cmp cl, '0'
+    jb .apply_sign
+    cmp cl, '9'
+    jb .apply_sign
+
+    ; result = result * 1- + (char - '0')
+    imul rax, 10
+    sub cl, '0'
+    movzx rcx, cl
+    add rax, rcx
+
+    inc rdi
+    jmp .loop_10
+
+ .base_16:
+    xor rax, rax
+    xor r8, r8
+
+    movzx rcx, byte [rdi]
+    cmp cl, '0'
+
+    cmp dl, 'x'
+    jne .loop_16
+    inc rdi
+    mov r8, 1
+
+ .loop_16:
+    movzx rcx, byte [rdi]
+
+
+ .done:
+    ret
