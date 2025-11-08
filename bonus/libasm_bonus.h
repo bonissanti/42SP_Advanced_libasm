77a3d5e909ef00a4aa7e8ebf9304fd3a18d74e9c
bonissanti <brunoonissanti@gmail.com>
2025-11-08 18:44:43 -0300

    fix: fixed problem in libasm_bonus.h

--- bonus/libasm_bonus.h
+++ bonus/libasm_bonus.h
@@ -1,16 +1,17 @@
 #ifndef LIBASM_BONUS_H
 #define LIBASM_BONUS_H
 
-extern int  ft_atoi_base(const char *str, char *base);
-extern void ft_list_sort(t_list **begin_list, int (*cmp)());
-extern int  ft_list_size(t_list *begin_list);
-extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
-extern void ft_list_push_front(t_list **begin_list, void *data);
-
 typedef struct s_list
 {
     void            *data;
     struct s_list   *next;
 }                   t_list;
+
+
+extern int  ft_atoi_base(const char *str, char *base);
+extern void ft_list_sort(t_list **begin_list, int (*cmp)());
+extern int  ft_list_size(t_list *begin_list);
+extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+extern void ft_list_push_front(t_list **begin_list, void *data);
 
 #endif //LIBASM_BONUS_H
